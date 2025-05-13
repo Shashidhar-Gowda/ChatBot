7c0913329550afdffa53fdcb6f90d59f312576a2
Shashidhar-Gowda <shashidhar.jagadeesh@brainlabsdigital.com>
2025-05-13 18:44:44 +0530

    Data migration changes

--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -23,7 +23,7 @@ class UploadedFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.FileField(upload_to='user_uploads/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    filename = models.CharField(max_length=255, blank=True, null=True)
+    # filename = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.file.name
